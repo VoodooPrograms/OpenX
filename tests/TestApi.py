1d31048f1971b42403db98f9cea9e031cf335e20
VoodooPrograms <arkanidius@gmail.com>
2020-03-21 18:43:44 +0100

    Prototype of unit tests

---
+++ tests/TestApi.py
@@ -0,0 +1,24 @@
+import unittest
+
+from src.Api import Api
+
+
+class TestApi(unittest.TestCase):
+
+    def setUp(self):
+        self.api = Api
+
+    def test_set_posts(self):
+        pass
+
+    def test_set_users(self):
+        pass
+
+    def test_count_posts_by_users(self):
+        pass
+
+    def test_not_unique_posts(self):
+        pass
+
+    def test_closest_user(self):
+        pass
---
+++ tests/TestNode.py
@@ -0,0 +1,18 @@
+import unittest
+
+from src.Node import Node, Tree
+
+
+class TestNode(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_sum(self):
+        pass
+
+    def test_avg(self):
+        pass
+
+    def test_mediana(self):
+        pass
