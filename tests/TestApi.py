eed5ae45ad1acb4607c4ddc42bf098aaeabcb013
VoodooPrograms <arkanidius@gmail.com>
2020-03-21 18:58:51 +0100

    Writing tests for every method in Api

--- tests/TestApi.py
+++ tests/TestApi.py
@@ -9,16 +9,26 @@ class TestApi(unittest.TestCase):
         self.api = Api
 
     def test_set_posts(self):
-        pass
+        self.api.set_posts()
+        self.assertIsNotNone(self.api.posts)
+        self.assertEqual(self.api.posts.status_code, 200)
 
     def test_set_users(self):
-        pass
+        self.api.set_users()
+        self.assertIsNotNone(self.api.users)
+        self.assertEqual(self.api.users.status_code, 200)
 
     def test_count_posts_by_users(self):
-        pass
+        result = self.api.count_posts_by_users()
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, str)
 
     def test_not_unique_posts(self):
-        pass
+        result = self.api.not_unique_posts()
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, list)
 
     def test_closest_user(self):
-        pass
+        result = self.api.closest_user()
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, dict)
