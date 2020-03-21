d1c5402fbff7d09366a8c86bcb433b05ba95d46d
VoodooPrograms <arkanidius@gmail.com>
2020-03-21 18:52:26 +0100

    Build tree from task

--- tests/TestNode.py
+++ tests/TestNode.py
@@ -6,7 +6,16 @@ from src.Node import Node, Tree
 class TestNode(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.n1 = Node(5)
+        self.n1.left = Node(3)
+        self.n1.right = Node(7)
+        self.n1.left.left = Node(2)
+        self.n1.left.right = Node(5)
+        self.n1.right.left = Node(1)
+        self.n1.right.right = Node(0)
+        self.n1.right.right.left = Node(2)
+        self.n1.right.right.right = Node(8)
+        self.n1.right.right.right.right = Node(5)
 
     def test_sum(self):
         pass
